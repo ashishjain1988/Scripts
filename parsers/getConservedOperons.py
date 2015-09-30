b6b10b606a5b2fca2480ee6f4729ebd32e5423c5
Ashish Jain <jain.ashishjain1@gmail.com>
2015-09-30 09:16:28 -0500

    Updated the retrieval of rpob gene in those genomes which have them as their misc_feature not as their CDS gene.

--- parsers/getConservedOperons.py
+++ parsers/getConservedOperons.py
@@ -32,21 +32,21 @@ def getConservedOperonsList(pickleObj):
     operonTotalDistDict = {}    
     for operon in operonDistanceDict:
         distance = 0
+        no_of_orgs = operonKeysDict[operon]
         for event,value in operonDistanceDict[operon].iteritems():
             distance +=value
-        operonTotalDistDict.update({operon:distance})
+        operonTotalDistDict.update({operon:(distance*1.0/no_of_orgs)})
     handle = open("conservedOperonsSorted.txt","w")
     for operon in sorted(operonTotalDistDict.items(), key=lambda x: x[1]):
         #print operon
         if(operonKeysDict[operon[0]] >=5):
-            print operonKeysDict[operon[0]]
             handle.write(str(operon))
             handle.write("\n")
     handle.close()
 
 def main():
     #print "Main"
-    event_dist = pickle.load(open("/home/jain/Gram_Positive_Bacteria_Study/Test_Run/gene_block_distance_matrices/event_dict.p"))
+    event_dist = pickle.load(open("/home/jain/Gram_Positive_Bacteria_Study/Test_Run_Test/gene_block_distance_matrices/event_dict.p"))
     getConservedOperonsList(event_dist)
 
 if __name__ == "__main__":
