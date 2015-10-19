8cc131de64b7ef660fbca6eadf106b09555b1576
Ashish Jain <jain.ashishjain1@gmail.com>
2015-10-19 15:47:16 -0500

    Updated code for Removing strains and plasmids genomes.

--- et2/filterOutStrains.py
+++ et2/filterOutStrains.py
@@ -16,7 +16,9 @@ def filterStrain(handle,outHandle):
         lineData = i.split("\t")
         strainName = lineData[0].strip();
         folderPath = lineData[1].strip();
-        print str(return_recursive_files(folderPath)) + "\n"
+        #print folderPath
+        #print str(return_recursive_files(folderPath)) + "\n"
+        print strainName
         for f in return_recursive_files(folderPath):
             seq_record = SeqIO.parse(open(f), "genbank").next()
             accession = seq_record.annotations['accessions'][0]
@@ -24,6 +26,8 @@ def filterStrain(handle,outHandle):
             organism_tmp_1 = re.sub('[\[\]]', "", organism_tmp)
             organism = '_'.join(organism_tmp_1.split('_')[:2])#+"_"+accession
             #print organism
+            if accession == 'NC_022759':
+                print "found"
             if(distinctSpeciesDict.has_key(organism)):
                 oldFilePath = distinctSpeciesDict[organism]
                 old_record = SeqIO.parse(open(oldFilePath), "genbank").next()
@@ -32,9 +36,11 @@ def filterStrain(handle,outHandle):
                     distinctSpeciesDict.update({organism:f})
             else:
                 distinctSpeciesDict.update({organism:f})
-    print len(distinctSpeciesDict)
+                if accession == 'NC_022759':
+                    print "not found" 
+    #print len(distinctSpeciesDict)
     for key,value in distinctSpeciesDict.iteritems():
-        outHandle.write(key+"\t"+value+"\n")            
+        outHandle.write(key+"\t"+os.path.dirname(value)+"\n")            
     
         
 def return_recursive_files(root_dir):
@@ -49,31 +55,53 @@ def return_recursive_files(root_dir):
 def return_recursive_dir_files(root_dir):
     result = []
     for path, dir_name, flist in os.walk(root_dir):
-        for f in flist:
+        for f in dir_name:
             fname = os.path.join(path, f)
-            if not os.path.isdir(fname):
+            if os.path.isdir(fname):
                 result.append(fname)
     return result
 
 def deletePlasmids(rootDir):
-    for path, dir_name, flist in os.walk(rootDir):
-        for file in flist:
-            if os.path.isdir(file):
+    #for path, dir_name, flist in os.walk(rootDir):
+        #fileOut = open("morethan1chr.txt","w") 
+        for file in return_recursive_dir_files(rootDir):
+            #print file
+            #if os.path.isdir(file):
+                #print file
                 files = return_recursive_files(file)
-                for f in files:
-                    seq_record = SeqIO.parse(open(f), "genbank").next()
-                    accession = seq_record.annotations['accessions'][0]
-                    organism_tmp = seq_record.annotations['organism'].replace(' ', '_')
-                    organism_tmp_1 = re.sub('[\[\]]', "", organism_tmp)
-                    organism = '_'.join(organism_tmp_1.split('_')[:2])#+"_"+accession
-                    
-
+                #print str(file) + str(files)
+                fileName = ""
+                if len(files) > 1:
+                    for f in files:
+                        #print f
+                        seq_record = SeqIO.parse(open(f), "genbank").next()
+                        accession = seq_record.annotations['accessions'][0]
+                        organism_tmp = seq_record.annotations['organism'].replace(' ', '_')
+                        organism_tmp_1 = re.sub('[\[\]]', "", organism_tmp)
+                        organism = '_'.join(organism_tmp_1.split('_')[:2])#+"_"+accession
+                        definition = seq_record.description;
+                        #print def
+                        if ("plasmid" not in definition):
+                            if fileName != "":
+                                print "error"
+                                print file.split("/")[4]
+                                #fileOut.write(file.split("/")[4]+"\n")
+                            fileName = accession+".gbk"
+                            #print fileName 
+                    for f in files:
+                        #print os.path.basename(f)
+                        if os.path.basename(f) != fileName:
+                            os.remove(f)
+                            #print "remove file "+f
+        #fileOut.close()                    
+                            
 def main():
-    handle = open("/home/jain/Gram_Positive_Bacteria_Study/Organisms_Lists_from_PATRIC/Proteobacteria/PatricOrgsGenomesMap.txt","r")
-    outHandle = open("/home/jain/Gram_Positive_Bacteria_Study/Organisms_Lists_from_PATRIC/Proteobacteria/filter_strain.txt","w")
-    filterStrain(handle,outHandle)
-    outHandle.close()
+    handle = open("/home/jain/Gram_Positive_Bacteria_Study/Organisms_Lists_from_PATRIC/Proteobacteria/newOrgListAfterStrainFiltering.txt","r")
+    #outHandle = open("/home/jain/Gram_Positive_Bacteria_Study/Organisms_Lists_from_PATRIC/Proteobacteria/filter_strain.txt","w")
+    #filterStrain(handle,outHandle)
+    #outHandle.close()
     handle.close()
+    deletePlasmids("/home/jain/Gram_Positive_Bacteria_Study/Proteobacteria_Genomes_Path_1")
     
 
 if __name__ == "__main__":
