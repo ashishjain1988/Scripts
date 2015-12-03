2977aef38dbefaa92bd92970b89482f1d99bccd2
Ashish Jain <jain.ashishjain1@gmail.com>
2015-12-03 17:43:30 -0600

    Fix the labelling of the heatmap with the exact values.

--- heatmap_creation/createHeatMapFromMatplotLib.py
+++ heatmap_creation/createHeatMapFromMatplotLib.py
@@ -1,29 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns;
 
+np.random.seed(0)
+sns.set()
 column_labels = list('ABCD')
 row_labels = list('WXYZ')
 
 x = ['E9.5 Male', 'E9.5 Female', 'E9.5 TGC']
-y = ['E9.5 TGC', 'E9.5 Female', 'E9.5 Male']
-#data = np.random.rand(4,4)
+y = ['E9.5 Male', 'E9.5 Female', 'E9.5 TGC']
 
 fig, ax = plt.subplots()
-data = pd.read_csv(open("/home/jain/heatmap_test_data.csv"), index_col=0)
-heatmap = ax.pcolor(data,cmap=plt.cm.Blues, alpha=0.8)
+#data = pd.read_csv(open("/home/jain/heatmap_test_data.csv"), index_col=0)
+data = np.genfromtxt('/home/jain/heatmap_test_data.csv', delimiter=',')
+#ax = sns.heatmap(data, annot=True)
+heatmap = plt.pcolor(data,cmap=plt.cm.Blues, alpha=0.8)
+plt.colorbar(heatmap)
 print data
 
-#nba = pd.read_csv(open("/home/jain/heatmap_test_data.csv"), index_col=0)
-# put the major ticks at the middle of each cell, notice "reverse" use of dimension
-#print (nba)
-#nba_norm = (nba - nba.mean()) / (nba.max() - nba.min())
-#nba_sort = nba_norm.sort('PTS', ascending=True)
-#print nba_sort
 ax.set_yticks(np.arange(data.shape[0])+0.5, minor=False)
 ax.set_xticks(np.arange(data.shape[1])+0.5, minor=False)
+#Labelling the groups of the heatmap
+ax.set_xticklabels(x)
+ax.set_yticklabels(y)
+#Labelling the heatmap with the Exact values.
+for y in range(data.shape[0]):
+    for x in range(data.shape[1]):
+        plt.text(x + 0.5, y + 0.5, '%d' % data[y, x],
+                 horizontalalignment='center',
+                 verticalalignment='center',
+                 )
 
-
-ax.set_xticklabels(x, minor=False)
-ax.set_yticklabels(y, minor=False)
 plt.show()
